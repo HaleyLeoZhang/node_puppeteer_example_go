fcc76016cece283212a6a79641124e593a6ce82f
云天河 <hlzblog@vip.qq.com>
2020-11-14 02:56:07 +0800

    更新结构体分层、个别Dao层逻辑

--- api/service/page_deatail.go
+++ api/service/page_deatail.go
@@ -6,11 +6,16 @@ import (
 	"github.com/HaleyLeoZhang/go-component/driver/xlog"
 	"github.com/HaleyLeoZhang/go-component/errgroup"
 	"github.com/spiegel-im-spiegel/logf"
+	"node_puppeteer_example_go/api/model/po"
 	"node_puppeteer_example_go/api/model/vo"
 )
 
 func (s *Service) PageDetail(ctx context.Context, param *vo.PageDetailParam) (res *vo.PageDetailResponse, err error) {
+	// 填充所有默认结构体，使得前端接收结构完整
 	res = &vo.PageDetailResponse{}
+	res.Comic = &po.Comic{}
+	res.NextPage = &po.ComicPage{}
+	res.Page = &po.ComicPage{}
 	err = nil
 
 	pageId := param.PageId
@@ -19,6 +24,8 @@ func (s *Service) PageDetail(ctx context.Context, param *vo.PageDetailParam) (re
 		xlog.Errorf("PageDetail.Step1.Error.%+v", err)
 		return nil, err
 	}
+	res.Page = currentPage
+
 	g := &errgroup.Group{}
 	g.GOMAXPROCS(2)
 	g.Go(func(context.Context) (errNil error) {
